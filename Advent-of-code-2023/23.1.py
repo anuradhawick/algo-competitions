f5353384121fcbdf655da48ae28f4614a671f3db
Anuradha <anuradhawick@gmail.com>
2023-12-26 04:44:28 +0000

    AoC 2023 day 23

---
+++ Advent-of-code-2023/23.1.py
@@ -0,0 +1,73 @@
+from collections import defaultdict
+import heapq as hq
+
+data = """
+#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#
+"""
+
+data = open('23.txt').read()
+
+matrix = [list(line) for line in data.strip().split("\n")]
+start = 0, matrix[0].index(".")
+end = len(matrix) - 1, matrix[-1].index(".")
+directions = dict()
+directions["."] = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+directions[">"] = [(0, 1)]
+directions["<"] = [(0, -1)]
+directions["^"] = [(-1, 0)]
+directions["v"] = [(1, 0)]
+
+# queue stores (parent, node) to account for four possible incomings
+queue = [(0, (None, start))]
+cost = {(None, start): 0}
+parent = {(None, start): (None, None)}
+
+while len(queue):
+    curr_cost, curr_state = hq.heappop(queue)
+    curr_parent, (r, c) = curr_state
+    curr_char = matrix[r][c]
+
+    for d in directions[curr_char]:
+        dr, dc = d
+        nr, nc = r + dr, c + dc
+
+        if 0 <= nr < len(matrix) and 0 <= nc < len(matrix[0]) and matrix[nr][nc] != "#":
+            new_cost = curr_cost - 1
+            new_state = ((r, c), (nr, nc))
+
+            if curr_state in parent and (nr, nc) == parent[curr_state][1]:
+                continue
+
+            if new_state not in cost or curr_cost < cost[new_state]:
+                cost[new_state] = new_cost
+                parent[new_state] = curr_state
+                hq.heappush(queue, (new_cost, new_state))
+
+            # print("Added", (nr, nc), pos_parent_dict[(nr, nc)])
+
+for k, v in cost.items():
+    p, s = k
+    if s==end:
+        print(s, abs(v))
---
+++ Advent-of-code-2023/23.2.py
@@ -0,0 +1,100 @@
+from tqdm import tqdm
+import networkx as nx
+import sys
+
+data = """
+#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#
+"""
+
+data = open("23.txt").read()
+
+matrix = [list(line) for line in data.strip().split("\n")]
+start = 0, matrix[0].index(".")
+end = len(matrix) - 1, matrix[-1].index(".")
+directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+edges = []
+
+for r, rd in enumerate(matrix):
+    for c, cd in enumerate(rd):
+        if cd == "#":
+            continue
+        for d in directions:
+            dr, dc = d
+            nr, nc = r + dr, c + dc
+            if (
+                0 <= nr < len(matrix)
+                and 0 <= nc < len(matrix[0])
+                and matrix[nr][nc] != "#"
+            ):
+                edge = (f"{r},{c}", f"{nr},{nc}")
+                edges.append(edge)
+                edge = (f"{nr},{nc}", f"{r},{c}")
+                edges.append(edge)
+
+G = nx.Graph(edges)
+start_node = ",".join(map(str, start))
+end_node = ",".join(map(str, end))
+edge_weights = dict()
+
+for u, v in G.edges:
+    edge_weights[(u, v)] = 1
+    edge_weights[(v, u)] = 1
+
+print("Initial nodes count", len(G.nodes))
+print("Initial edges count", len(G.edges))
+
+
+def compress(G: nx.Graph):
+    for node in list(G.nodes):
+        neighbours = list(G.neighbors(node))
+
+        if len(neighbours) == 2:
+            u, v = neighbours
+            replacements = [(u, v), (v, u)]
+            edge_weights[(u, v)] = edge_weights[(u, node)] + edge_weights[(node, v)]
+            edge_weights[(v, u)] = edge_weights[(v, node)] + edge_weights[(node, u)]
+            G.remove_node(node)
+            G.add_edges_from(replacements)
+
+
+compress(G)
+print("Simplified nodes count", len(G.nodes))
+print("Simplified edges count", len(G.edges))
+pbar = tqdm()
+longest = 0
+
+for path in nx.all_simple_paths(G, source=start_node, target=end_node):
+    prev = path[0]
+    dist = 0
+    for nxt in path[1:]:
+        dist += edge_weights[(prev, nxt)]
+        prev = nxt
+    longest = max(longest, dist)
+    pbar.set_description(f"Longest so far {longest}")
+    pbar.update()
+print(longest)
+
+# Answer 6498 ~ 2 minutes
